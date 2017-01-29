add5b9e84ba02fd60496f09e0d4e8acddc863e67
karthikr1901 <karthikr1901@gmail.com>
2017-01-29 22:54:58 +0500

    Python 1 code run

--- Python1/simple.py
+++ Python1/simple.py
@@ -8,3 +8,84 @@
 # You have to parse only the first 10 data lines in this exercise,
 # so the returned list should have 10 entries!
 
+import os
+import csv
+import pprint
+
+
+
+DATADIR = ""
+
+DATAFILE = "beatles-diskography.csv"
+
+
+
+
+def parse_file(datafile):
+
+    #data2=[]
+    #count2=0
+    #with open(datafile, "r") as f2:
+    #    r2 = csv.DictReader(f2)
+    #    for line2 in r2:
+    #        if(count2<10):
+    #            data2.append(line2)
+    #            count2+=1;
+
+    #    print(data2)
+
+    #return data2
+
+
+    data = []
+
+    with open(datafile, "r") as f:
+
+        header = f.readline().split(",")
+        counter = 0
+        for line in f:   
+                
+            if counter == 10:
+                break
+
+            fields = line.split(",")
+            entry = {}
+
+            for i, value in enumerate(fields):
+                entry[header[i].strip()] = value.strip()
+
+            data.append(entry)
+            counter += 1
+
+        #print (data)
+        #input()
+
+        return data
+
+
+
+def test():
+
+    # a simple test of your implemetation
+
+    datafile = os.path.join(DATADIR, DATAFILE)
+
+    d = parse_file(datafile)
+
+
+    firstline = {'Title': 'Please Please Me', 'UK Chart Position': '1', 'Label': 'Parlophone(UK)', 'Released': '22 March 1963', 'US Chart Position': '-', 'RIAA Certification': 'Platinum', 'BPI Certification': 'Gold'}
+
+
+    tenthline = {'Title': '', 'UK Chart Position': '1', 'Label': 'Parlophone(UK)', 'Released': '10 July 1964', 'US Chart Position': '-', 'RIAA Certification': '', 'BPI Certification': 'Gold'}
+
+
+
+    assert d[0] == firstline
+
+    assert d[9] == tenthline
+
+    
+
+
+
+test()
